3c21bf80314ae9d7aeded33c7c7923d2a136d85c
Raymond Davidson <ray@Raymonds-MacBook-Pro-2.local>
2025-09-02 20:33:33 -0700

    character button fixes and button color fix

--- src/components/ClassButtons.jsx
+++ src/components/ClassButtons.jsx
@@ -11,167 +11,114 @@ export default function ClassButtons ({
   onClick = () => {},
   onAdd = () => {},
 }) {
-    return (
-      <div className="class-buttons">
-        <div className="class-tab-container">
-          {CHARS_ONE.map((character_class, index) => {
-            const ci = character_class.instance;
-            return (
+  return (
+    <div className="class-buttons">
+      <div className="class-tab-container">
+        {CHARS_ONE.map((character_class, index) => {
+          const ci = character_class.instance;
+          return (
+            <div
+              key={`key_class_button_${index}`}
+              className="button-container"
+            >
               <div
-                key={`key_class_button_${index}`}
-                className="button-container"
+                className="name-container"
+                onClick={() => {
+                  onClick({
+                    ...CHARS_ONE[index],
+                    index,
+                  });
+                }}
+                style={{
+                  opacity: typeof currentIndex === "number" ? currentIndex === index ? 1 : 0.5 : 0.5,
+                }}
               >
-                <div
-                  className="name-container"
-                  onClick={() => {
-                    onClick({
-                      ...CLASSES[index],
-                      index,
-                    });
-                  }}
-                  style={{
-                    opacity: typeof currentIndex === "number" ? currentIndex === index ? 1 : 0.5 : 0.5,
-                  }}
-                >
-                  <button
-                    key={`class_button_${ci.class}`}
-                    className="class-button"
-                    style={textOutline(ci.color, 15)}
-                  >
-                    {ci.class}
-                  </button>
-                  <div
-                    className="class-arrow"
-                    style={{
-                      color: ci.color,
-                    }}
-                  />
-                </div>
-
                 <button
-                  key={`class_button_add_${ci.class}`}
-                  className="class-button-add"
-                  onClick={() => {
-                    onAdd({
-                      ...CLASSES[index],
-                      index,
-                    });
-                  }}
-                  style={textOutline(ci.color, 1)}
+                  key={`class_button_${ci.class}`}
+                  className="class-button"
+                  style={textOutline(ci.color, 15)}
                 >
-                  +
+                  {ci.class}
                 </button>
+                <div
+                  className="class-arrow"
+                  style={{
+                    color: ci.color,
+                  }}
+                />
               </div>
-            );
-          })}
-        </div>
-        <div className="class-tab-container">
-          {CHARS_TWO.map((character_class, index) => {
-            const ci = character_class.instance;
-            return (
-              <div
-                key={`key_class_button_${index}`}
-                className="button-container"
+
+              <button
+                key={`class_button_add_${ci.class}`}
+                className="class-button-add"
+                onClick={() => {
+                  onAdd({
+                    ...CHARS_ONE[index],
+                    index,
+                  });
+                }}
+                style={textOutline(ci.color, 1)}
               >
-                <div
-                  className="name-container"
-                  onClick={() => {
-                    onClick({
-                      ...CLASSES[index],
-                      index,
-                    });
-                  }}
-                  style={{
-                    opacity: typeof currentIndex === "number" ? currentIndex === index ? 1 : 0.5 : 0.5,
-                  }}
-                >
-                  <button
-                    key={`class_button_${ci.class}`}
-                    className="class-button"
-                    style={textOutline(ci.color, 15)}
-                  >
-                    {ci.class}
-                  </button>
-                  <div
-                    className="class-arrow"
-                    style={{
-                      color: ci.color,
-                    }}
-                  />
-                </div>
-
-                <button
-                  key={`class_button_add_${ci.class}`}
-                  className="class-button-add"
-                  onClick={() => {
-                    onAdd({
-                      ...CLASSES[index],
-                      index,
-                    });
-                  }}
-                  style={textOutline(ci.color, 1)}
-                >
-                  +
-                </button>
-              </div>
-            );
-          })}
-        </div>
+                +
+              </button>
+            </div>
+          );
+        })}
       </div>
-    );
 
-  // return (
-  //     <div className="class-buttons">
-  //       {CLASSES.map((character_class, index) => {
-  //         const ci = character_class.instance;
-  //         return (
-  //           <div
-  //             key={`key_class_button_${index}`}
-  //             className="button-container"
-  //           >
-  //             <div
-  //               className="name-container"
-  //               onClick={() => {
-  //                 onClick({
-  //                   ...CLASSES[index],
-  //                   index,
-  //                 });
-  //               }}
-  //               style={{
-  //                 opacity: typeof currentIndex === "number" ? currentIndex === index ? 1 : 0.5 : 0.5,
-  //               }}
-  //             >
-  //               <button
-  //                 key={`class_button_${ci.class}`}
-  //                 className="class-button"
-  //                 style={textOutline(ci.color, 15)}
-  //               >
-  //                 {ci.class}
-  //               </button>
-  //               <div
-  //                 className="class-arrow"
-  //                 style={{
-  //                   color: ci.color,
-  //                 }}
-  //               />
-  //             </div>
+      <div className="class-tab-container">
+        {CHARS_TWO.map((character_class, i) => {
+          const ci = character_class.instance;
+          const index = i + CHARS_ONE.length;
+          return (
+            <div
+              key={`key_class_button_${index}`}
+              className="button-container"
+            >
+              <div
+                className="name-container"
+                onClick={() => {
+                  onClick({
+                    ...CHARS_TWO[i],
+                    index,
+                  });
+                }}
+                style={{
+                  opacity: typeof currentIndex === "number" ? currentIndex === index ? 1 : 0.5 : 0.5,
+                }}
+              >
+                <button
+                  key={`class_button_${ci.class}`}
+                  className="class-button"
+                  style={textOutline(ci.color, 15)}
+                >
+                  {ci.class}
+                </button>
+                <div
+                  className="class-arrow"
+                  style={{
+                    color: ci.color,
+                  }}
+                />
+              </div>
 
-  //             <button
-  //               key={`class_button_add_${ci.class}`}
-  //               className="class-button-add"
-  //               onClick={() => {
-  //                 onAdd({
-  //                   ...CLASSES[index],
-  //                   index,
-  //                 });
-  //               }}
-  //               style={textOutline(ci.color, 1)}
-  //             >
-  //               +
-  //             </button>
-  //           </div>
-  //         );
-  //       })}
-  //     </div>
-  // );
+              <button
+                key={`class_button_add_${ci.class}`}
+                className="class-button-add"
+                onClick={() => {
+                  onAdd({
+                    ...CHARS_TWO[i],
+                    index,
+                  });
+                }}
+                style={textOutline(ci.color, 1)}
+              >
+                +
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
